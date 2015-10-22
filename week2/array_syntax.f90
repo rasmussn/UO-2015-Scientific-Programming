eba5b451efaaa7de42292a39a8e87b4d557cbe48
Craig Rasmussen <rasmus@uoregon.edu>
2015-10-22 08:06:28 -0700

    Added comments and computation.

--- week2/array_syntax.f90
+++ week2/array_syntax.f90
@@ -1,5 +1,22 @@
+!!!!!
+!  This simple example shows the power of array syntax in Fortran.
+!
+!!!!!
 program array_syntax
+   implicit none
+   integer :: A(5), B(5), C(5)
 
-   print *, "Hellno"
+!! initialization of arrays
+!
+   A = 1
+   B = 2
+
+!! computation using arrays
+!
+   C = A + 13*B
+
+!! dump out the results (note these really are arrays)
+!
+   print *, "C: ", C
 
 end program array_syntax
